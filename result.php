af82e36df4592c99c1cf9a900c7f8802d8a6c1a4
arnel marquez <arnelmarquez@users.noreply.github.com>
2015-05-23 13:23:21 +0200

    Create result.php

---
+++ result.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="google-site-verification" content="SirOsqrHyysUoeGyVFgK1N4qfQR-Sw39IqjSCgxG9kI" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" href="favicon.ico">
+<title>DitoNaTayo.com Search on the Web</title>
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60176674-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+<style>
+body{
+	height:auto;
+	font-family:"Helvetica Neue", Arial, sans-serif;
+	margin:0 15px;
+}
+a{
+	text-decoration:none;
+	color:#60C6E1;
+}
+a:hover{
+	text-decoration:none;
+	color:#666666;
+}
+#boton{
+	padding:3px 15px;
+	height:35px;
+	background: #8acadf;
+  	background: -moz-linear-gradient(top, #8acadf 0%, #70c2dc 5%, #23a1c9 100%);
+  	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#8acadf), color-stop(5%,#70c2dc), color-stop(100%,#23a1c9));
+  	background: -o-linear-gradient(top, #8acadf 0%,#70c2dc 5%,#23a1c9 100%);
+  	background: -ms-linear-gradient(top, #8acadf 0%,#70c2dc 5%,#23a1c9 100%);
+  	filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#8acadf', endColorstr='#23a1c9',GradientType=0);
+  	background: linear-gradient(top, #8acadf 0%,#70c2dc 5%,#23a1c9 100%);
+	border-radius:6px;
+	color:#FFFFFF;
+	border: 0 none;
+	font-size:16px;
+	margin:5px 0;
+}
+#searchbox{
+	padding:6px;
+	height:20px;
+	max-width:500px;
+	min-width:100px;
+	width:80%;
+	font-size:16px;
+	border:#60C6E1 1px solid;
+	margin:5px 0;
+}
+</style>
+
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4f3f9dc36c56bd9b" async="async"></script>
+
+</head>
+
+<body>
+<div style="width:100%; height:auto; text-align:right; padding:10px 0;"><a href="http://search.ditonatayo.com">Home</a> &middot; <a href="http://ditonatayo.com" target="_blank">Web</a> &middot; <a href="http://1050532.spreadshirt.com/" target="_blank">Shop</a> &middot; <a href="http://ditonatayo.com/blog/" target="_blank">Blog</a> &middot; <a href="http://forum.ditonatayo.com/" target="_blank">Forum</a> &middot; <a href="https://www.facebook.com/ditonatayocom" target="_blank">Facebook</a> &middot; <a href="https://twitter.com/Dito_Na_Tayo" target="_blank">Twitter</a>
+</div>
+
+<div style="width:100%; text-align:center;">
+    <div style="width:100%; height:80px; text-align:left; display:block;">
+        <a href="/index.php"><img src="/images/logo-small.jpg" alt="dito na tayo" style=" height:80px; float:left; margin-right:15px; margin-top:-30px;" border="0" /></a>
+        <form action="/result.php" id="cse-search-box">
+          <div style="margin: auto 0; float:left; width:80%;">
+            <input type="hidden" name="cx" value="partner-pub-7795143638096204:9539633717" />
+            <input type="hidden" name="cof" value="FORID:10" />
+            <input type="hidden" name="ie" value="UTF-8" />
+            <input id="searchbox" type="text" name="q" size="55" required />
+            <input id="boton" type="submit" name="sa" value="Search" />
+          </div>
+        </form>
+	</div>
+    <div style="width:95%; text-align:left; margin:0 3%;">
+
+    <div id="cse-search-results"></div>
+    <script type="text/javascript">
+      var googleSearchIframeName = "cse-search-results";
+      var googleSearchFormName = "cse-search-box";
+      var googleSearchFrameWidth = 900;
+      var googleSearchDomain = "www.google.com";
+      var googleSearchPath = "/cse";
+    </script>
+    <script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
+    
+    
+    <script type="text/javascript" src="http://www.google.com/cse/query_renderer.js"></script>
+    <div id="queries"></div>
+    <script src="http://www.google.com/cse/api/partner-pub-7795143638096204/cse/9539633717/queries/js?oe=UTF-8&amp;callback=(new+PopularQueryRenderer(document.getElementById(%22queries%22))).render"></script>
+    </div>
+
+</div>
+
+<!-- Google Code for Remarketing Tag -->
+<!--------------------------------------------------
+Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
+--------------------------------------------------->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 956710688;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/956710688/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+</body>
+</html>
